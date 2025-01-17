3092427c060e839bfef6c147a2447e3736f7727f
Piyushrathoree <meonpath008@gmail.com>
2025-01-17 17:01:28 +0530

    solved the database connection error

--- server/db/testDB.js
+++ server/db/testDB.js
@@ -1,3 +1,15 @@
-import connectDB from "./db.js"; // Update with your file structure
+import mongoose from "mongoose";
 
-connectDB();
+function connectDB() {
+    const URL = `${process.env.MONGODB_URI}/${process.env.DB_NAME}`;
+    mongoose.connection.on("connected", () => console.log("Database connected successfully!"));
+    mongoose.connection.on("error", (err) => console.error("Database connection error:", err.message));
+    mongoose.connection.on("disconnected", () => console.log("Database disconnected!"));
+
+    mongoose
+        .connect(URL)
+        .then(() => console.log("MongoDB Connected"))
+        .catch((error) => console.error("Initial connection error:", error.message));
+}
+
+export default connectDB;

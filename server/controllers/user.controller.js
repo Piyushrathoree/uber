ca7a11a35c5323d2cad635c4f2fb5a961cd5784f
Piyushrathoree <meonpath008@gmail.com>
2025-01-18 01:08:01 +0530

    completed the register and login controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,18 +1,15 @@
-import { log } from "console";
-import {User} from "../models/user.model.js";
+import { log } from "node:console";
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 const registerUser = async (req, res) => {
-    const {firstName, lastName, email, password}  = req.body;
+    const { firstName, lastName, email, password } = req.body;
 
-    console.log(req.body); // Add this before destructuring
-
-    
-    
     if (!firstName || !lastName || !email || !password) {
         return res.status(400).json({ msg: "Please fill all fields" });
     }
     // check if email is already registered
-    const duplicateEmail = await User.findOne ({ email });
+    const duplicateEmail = await User.findOne({ email });
     if (duplicateEmail) {
         return res.status(400).json({ msg: "Email already registered" });
     }
@@ -24,22 +21,38 @@ const registerUser = async (req, res) => {
         firstName,
         lastName,
         email,
-        password: hashPassword, 
+        password: hashPassword,
     });
     try {
         await newUser.save();
-        res.status(201).json({ msg: "User registered successfully" });
+        res.status(201).json(newUser, { msg: "User registered successfully" });
     } catch (error) {
         console.error(error.message);
         return res.status(500).json({ msg: "Server Error" });
     }
 };
 
-const sample = (req , res )=>{
-    res.json({message: "sample endpoint"})
-}
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    if (!(email || password)) {
+        return res.status(400).json({ msg: "Please fill all fields" });
+    }
+    const user = await User.findOne({ email }).select("-password");
+    if (!user) {
+        return res.status(400).json({ msg: "invalid username or password" });
+    }
 
-export {
-    registerUser,
-    sample
-}
+    const correctPass = await User.comparePass(password);
+    console.log(correctPass);
+
+    if (!correctPass) {
+        return res.status(400).json({ msg: "invalid username or password" });
+    }
+
+    const token = await User.generateToken();
+    console.log(token);
+
+    res.status(200).json({ token , user });
+};
+
+export { registerUser, loginUser };
